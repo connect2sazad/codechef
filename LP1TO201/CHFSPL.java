6142f301c9d0e6c59afae259f8b67ed4397dd5b6
Sazad Ahemad <mail2sazad@gmail.com>
2022-03-17 18:04:16 +0530

    edit 2

--- LP1TO201/CHFSPL.java
+++ LP1TO201/CHFSPL.java
@@ -5,28 +5,30 @@ import java.util.*;
 // import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class WEIGHTBL {
+class CHFSPL {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner z = new Scanner(System.in);
         int T = z.nextInt();
-        if (T >= 1 && T <= 100000)
+        if (T >= 1 && T <= test_range(4))
             for (int i = 0; i < T; i++)
                 code(z);
         z.close();
     }
 
-    static void code(Scanner z) {
-        int w1 = z.nextInt();
-        int w2 = z.nextInt();
-        int x1 = z.nextInt();
-        int x2 = z.nextInt();
-        int M = z.nextInt();
-        if (w1 >= 1 && w1 <= w2 && w2 <= 100 && x1 >= 0 && x1 <= x2 && x2 <= 100 && M >= 1 && M <= 10) {
-           int dw = w2-w1;
-           if(dw>=M*x1 && dw<=M*x2)
-               System.out.println(1);
-           else
-               System.out.println(0);
+    static int test_range(int pow){
+        int ans = 1;
+        for(int i=1;i<=pow;i++){
+            ans*=10;
         }
+        return ans;
+    }
+
+    static void code(Scanner z) {
+        int powers[] = new int[3];
+        for(int i=0;i<3;i++){
+            powers[i] = z.nextInt();
+        }
+        Arrays.sort(powers);
+        System.out.println(powers[1]+powers[2]);
     }
 }
---
+++ LP1TO201/MANYSUMS.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+// import java.lang.*;
+// import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class MANYSUMS {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner z = new Scanner(System.in);
+        int T = z.nextInt();
+        for (int i = 0; i < T; i++)
+            code(z);
+        z.close();
+    }
+
+    static void code(Scanner z) {
+        int N = z.nextInt();
+        int S = (N*(N+1))/2;
+        if(S%2==0)
+            System.out.println(N);
+        else
+            System.out.println(N-1);
+    }
+}
